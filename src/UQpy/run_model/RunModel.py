2fca4abf7145670e342a6165fb338dc591ad913b
Daniel Luder <daniel@luder.ch>
2024-07-27 14:42:00 +0200

    added correct command to print function

--- src/UQpy/run_model/RunModel.py
+++ src/UQpy/run_model/RunModel.py
@@ -201,7 +201,7 @@ class RunModel:
             pickle.dump(self.samples, filehandle)
             
         if self.run_type is RunType.LOCAL:        
-            print(f"mpirun python -m "
+            print(f"mpirun -np 4 python -m "
                   f"UQpy.run_model.model_execution.ParallelExecution {self.n_existing_simulations} "
                   f"{self.n_new_simulations}")
             
